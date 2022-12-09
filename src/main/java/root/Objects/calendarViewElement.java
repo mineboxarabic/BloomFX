1f28f73e7086a8e87d860b68a6596963e52becfd
Yassin YOUNES <mineboxarabic@gmail.com>
2022-12-09 11:58:08 +0100

    sdsd

--- src/main/java/root/Objects/calendarViewElement.java
+++ src/main/java/root/Objects/calendarViewElement.java
@@ -8,8 +8,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseButton;
@@ -25,28 +27,90 @@ public class calendarViewElement extends VBox
     public Button priorityTaskColor;
     public Label titleTask;
     public Label dateTask;
+    public Label descTask;
     public CheckBox checkBoxTask;
-    
+    void taskEdited(){
+        Popup popup = new Popup();
+        VBox content = new VBox();
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("uiFXML/Popupv2.fxml"));
+            Parent root = loader.load();
+            content.getChildren().add(root);
+            Scene scene = new Scene(content);
+            Button saveButton = (Button) scene.lookup("#saveEditedButton");
+            Button cancelButton = (Button) scene.lookup("#cancelEditedButton");
+            TextField titleField = (TextField) scene.lookup("#titleEdited");
+            TextArea DescArea = (TextArea) scene.lookup("#descEdited");
+            DatePicker datePicker = (DatePicker) scene.lookup("#dateEdited");
+            ComboBox<String> priorityComboBox = (ComboBox) scene.lookup("#PriorityEdited");
+            Button closeButton = (Button) root.lookup("#closeButton");
+            priorityComboBox.getItems().addAll("Low", "Medium", "High");
+            titleField.setText(currentTask.getTitle());
+            DescArea.setText(currentTask.getDescription());
+            datePicker.setValue(currentTask.getDate());
+            priorityComboBox.getSelectionModel().select(currentTask.getPriority());
+            closeButton.setOnMouseClicked(e -> {
+                popup.hide();
+            });
+            saveButton.setOnMouseClicked(e -> {
+                currentTask.setTitle(titleField.getText());
+                currentTask.setDescription(DescArea.getText());
+                currentTask.setPriority(priorityComboBox.getSelectionModel().getSelectedIndex());
+                currentTask.setDate(datePicker.getValue());
+                titleTask.setText(currentTask.getTitle());
+                descTask.setText(currentTask.getDescription());
+                int priority = currentTask.getPriority();
+                switch(priority){
+                    case -1:
+                        priorityTaskColor.setStyle("-fx-background-color: gray");
+                        break;
+                    case 0:
+                        priorityTaskColor.setStyle("-fx-background-color: #00FF00");
+                        break;
+                    case 1:
+                        priorityTaskColor.setStyle("-fx-background-color: #FFFF00");
+                        break;
+                    case 2:
+                        priorityTaskColor.setStyle("-fx-background-color: #FF0000");
+                        break;
+                }
+                popup.hide();
+            });
+            cancelButton.setOnMouseClicked(e -> {
+                popup.hide();
+            });
+            popup.getContent().add(scene.getRoot());
+            popup.show(this.getScene().getWindow());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
     public calendarViewElement(Task task) throws Exception{
         isDragging = false;
-        setStyle("-fx-background-color: white;");
+        setStyle("-fx-background-color: white; -fx-cursor: hand;");
         currentTask = task;
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("uiFXML/CalendarViewElement.fxml"));
         root = fxmlLoader.load();
         priorityTaskColor = (Button) root.lookup("#priorityTaskColor");
         titleTask = (Label) root.lookup("#titleTask");
         checkBoxTask = (CheckBox) root.lookup("#checkBoxTask");
-        
+        descTask = (Label) root.lookup("#descTask");
+        descTask.setText(currentTask.getDescription());
         titleTask.setText(currentTask.getTitle());
         checkBoxTask.setSelected(currentTask.getStatus());
-        switch(priorityTaskColor.getText()){
-            case "Low":
+        int priority = currentTask.getPriority();
+        switch(priority){
+            case -1:
+                priorityTaskColor.setStyle("-fx-background-color: gray");
+                break;
+            case 0:
                 priorityTaskColor.setStyle("-fx-background-color: #00FF00");
                 break;
-            case "Medium":
+            case 1:
                 priorityTaskColor.setStyle("-fx-background-color: #FFFF00");
                 break;
-            case "High":
+            case 2:
                 priorityTaskColor.setStyle("-fx-background-color: #FF0000");
                 break;
         }
@@ -63,8 +127,23 @@ public class calendarViewElement extends VBox
                     setStyle("-fx-background-color: #ffffff;");
                 }
             }
+            else {
+                ContextMenu contextMenu = new ContextMenu();
+                MenuItem edit = new MenuItem("Edit");
+                MenuItem delete = new MenuItem("Delete");
+                edit.setOnAction((e1) -> {
+                    taskEdited();
+                });
+                delete.setOnAction((e1) -> {
+                    this.getParent().getChildrenUnmodifiable().remove(this);
+                });
+                contextMenu.getItems().addAll(edit, delete);
+                contextMenu.show(this, e.getScreenX(), e.getScreenY());
+
+            }
         });
         this.getChildren().add(root);
+
 
 
     }
--- src/main/java/root/Views/calendarView.java
+++ src/main/java/root/Views/calendarView.java
@@ -12,6 +12,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.DatePicker;
@@ -28,41 +29,16 @@ import javafx.stage.Window;
 import root.Objects.*;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.event.EventTarget;
 import javafx.fxml.*;
-public class calendarView implements Initializable
-{
+
+public class calendarView implements Initializable {
     Task currentTask;
     Boolean isDragging = false;
     double xOffset = 0;
     double yOffset = 0;
-    /*public void getInfo(Parent day) throws Exception{
-        Popup popup = new Popup();
-        VBox content = new VBox();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("uiFXML/Popupv2.fxml"));
-        Parent root = loader.load();
-        content.getChildren().add(root);
-        Scene scene = new Scene(content);
-        Button saveButton = (Button) scene.lookup("#saveButton");
-        Button cancelButton = (Button) scene.lookup("#cancelButton");
-        TextField titleField = (TextField) scene.lookup("#titleField");
-        TextArea DescArea = (TextArea) scene.lookup("#DescArea");
-        DatePicker datePicker = (DatePicker) scene.lookup("#datePicker");
-        ComboBox<String> priorityComboBox = (ComboBox) scene.lookup("#priorityComboBox");
-        priorityComboBox.getItems().addAll("Low", "Medium", "High");
-
-        datePicker.show();
-        popup.show();
-        saveButton.setOnMouseClicked(e -> {
-            currentTask.setTitle(titleField.getText());
-            currentTask.setDescription(DescArea.getText());
-            currentTask.setPriority(priorityComboBox.getSelectionModel().getSelectedIndex());
-            currentTask.setDate(datePicker.getValue());
-            popup.hide();
-        });
-        cancelButton.setOnMouseClicked(e -> {
-            popup.hide();
-        });
-    }*/
+    Node dayEvent;
+    ContextMenu contextMenu;
     @FXML
     VBox mainContainer;
     @FXML
@@ -73,35 +49,61 @@ public class calendarView implements Initializable
     public VBox sunDayContainer;
     @FXML
     public VBox monDayContainer;
-    void getInfo(VBox day) throws IOException{
-            Popup popup = new Popup();
-            VBox content = new VBox();
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("uiFXML/Popupv2.fxml"));
-            Parent root = loader.load();
-            content.getChildren().add(root);
-            Scene scene = new Scene(content);
-            Button saveButton = (Button) scene.lookup("#saveButton");
-            Button cancelButton = (Button) scene.lookup("#cancelButton");
-            TextField titleField = (TextField) scene.lookup("#titleField");
-            TextArea DescArea = (TextArea) scene.lookup("#DescArea");
-            DatePicker datePicker = (DatePicker) scene.lookup("#datePicker");
-            ComboBox<String> priorityComboBox = (ComboBox) scene.lookup("#priorityComboBox");
-            priorityComboBox.getItems().addAll("Low", "Medium", "High");
-            datePicker.show();
-            saveButton.setOnMouseClicked(e -> {
-                currentTask.setTitle(titleField.getText());
-                currentTask.setDescription(DescArea.getText());
-                currentTask.setPriority(priorityComboBox.getSelectionModel().getSelectedIndex());
-                currentTask.setDate(datePicker.getValue());
+
+    void getInfo(VBox theDay) throws IOException {
+        Popup popup = new Popup();
+        popup.setAutoHide(true);
+        popup.setAutoFix(true);
+        popup.setHideOnEscape(true);
+        VBox content = new VBox();
+        content.setStyle("-fx-background-color: #ffffff;");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("uiFXML/Popupv2.fxml"));
+        Parent root = loader.load();
+        Button saveButton = (Button) root.lookup("#saveEditedButton");
+        Button cancelButton = (Button) root.lookup("#cancelEditedButton");
+        TextField titleField = (TextField) root.lookup("#titleEdited");
+        TextArea DescArea = (TextArea) root.lookup("#descEdited");
+        DatePicker datePicker = (DatePicker) root.lookup("#dateEdited");
+        ComboBox<String> priorityComboBox = (ComboBox) root.lookup("#PriorityEdited");
+        HBox categoriesEdited = (HBox) root.lookup("#categoriesEdited");
+        Button closeButton = (Button) root.lookup("#closeButton");
+        priorityComboBox.getItems().addAll("Low", "Medium", "High");
+        content.getChildren().add(root);
+        popup.getContent().add(content);
+        saveButton.setOnMouseClicked(e -> {
+            try {
+                Vector<String> categories = new Vector<String>();
+                for (Node task : categoriesEdited.getChildren()) {
+                    CheckBox category = (CheckBox) task;
+                    if (category.isSelected()) {
+                        categories.add(category.getText());
+                    }
+                }
+                String title = titleField.getText();
+                String description = DescArea.getText();
+                System.out.println(description);
+                int priority = priorityComboBox.getSelectionModel().getSelectedIndex();
+                LocalDate date = datePicker.getValue();
+
+                Task task = new Task(title, description, date, "12", priority, categories);
+                addTask(task, theDay);
                 popup.hide();
-            });
-            cancelButton.setOnMouseClicked(e -> {
-                popup.hide();
-            });
-            Window window = day.getScene().getWindow();
-            popup.show(window, window.getX() + window.getWidth() / 2, window.getY() + window.getHeight() / 2);
-            
+            } catch (Exception ex) {
+                System.out.println(ex);
+            }
+
+        });
+        cancelButton.setOnMouseClicked(e -> {
+            popup.hide();
+        });
+        closeButton.setOnMouseClicked(e -> {
+            popup.hide();
+        });
+        // get the mouse position
+        popup.show(theDay.getScene().getWindow());
+
     }
+
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         try
@@ -132,11 +134,12 @@ public class calendarView implements Initializable
                         
                     }
                 });
-
                day.setOnMouseClicked(e -> {
                      if(e.getButton() == MouseButton.PRIMARY)
                      {
                         try{
+                        if(contextMenu != null)
+                            contextMenu.hide();
                             if(calendarViewElement.selected != null){
                                 if(calendarViewElement.selected.isDragging && !theDay.getChildren().contains(calendarViewElement.selected)){
                                     theDay.getChildren().add(calendarViewElement.selected);
@@ -148,54 +151,72 @@ public class calendarView implements Initializable
                          
                      }
                      else{
-                        ContextMenu contextMenu = new ContextMenu();
-                        MenuItem addTask = new MenuItem("Add Task");
-
-                        addTask.setOnAction(e2 -> {
-                            Vector<String> categories = new Vector<>();
-                            try{
-                                getInfo(theDay);
-                                //categories.add("test");
-                                //Task task = new Task("test", "test", LocalDate.now(), "fe",1,false,categories);
-                                //addTask(task, theDay);
-                            }
-                            catch(Exception ex){
-                                System.out.println(ex);
-                            }
+                        try{
+                            dayEvent = (VBox) e.getTarget();
+                            System.out.println(dayEvent.getId());
+                        }catch(Exception ex){
+                            dayEvent = (Label) e.getTarget();
+                        }
+                            if((dayEvent.getId().equals("sunDayContainer")) 
+                            || (dayEvent.getId().equals("monDayContainer") 
+                            || (dayEvent.getId().equals("tuesDayContainer")) 
+                            || (dayEvent.getId().equals("wednesDayContainer")) 
+                            || (dayEvent.getId().equals("thursDayContainer")) 
+                            || (dayEvent.getId().equals("friDayContainer")) 
+                            || (dayEvent.getId().equals("saturDayContainer")))){
+                            if(contextMenu != null)
+                                contextMenu.hide();
                             
+                            contextMenu = new ContextMenu();
+                            MenuItem addTask = new MenuItem("Add Task");
 
-                        });
-                        contextMenu.getItems().add(addTask);
-                        contextMenu.show(day, e.getScreenX(), e.getScreenY());
-
-                     }
-                });
+                            addTask.setOnAction(e2 -> {
+                            
+                                    Vector<String> categories = new Vector<>();
+                                    try{
+                                        getInfo(theDay);
+                                    }
+                                    catch(Exception ex){
+                                        System.out.println(ex);
+                                    }
+                            });
+                            contextMenu.getItems().add(addTask);
+                            contextMenu.show(day, e.getScreenX(), e.getScreenY());
+                        }
+                        
+                }
+                
             }
-        }
-        catch(Exception e){
-            System.out.println(e);
+            );
         }
     }
+    catch(Exception e){
+        System.out.println(e);
+    }
 
-    void addTask(Task task,VBox day)
-    {
-        try{
+
+
+
+    }
+
+    void addTask(Task task, VBox day) {
+        try {
             calendarViewElement element = new calendarViewElement(task);
             calendarViewElement.selected = element;
             day.getChildren().add(element);
 
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
-    
+
     }
+
     @FXML
-    void addTask(){
+    void addTask() {
         Vector<String> categories = new Vector<>();
         categories.add("test");
-        Task task = new Task("test", "test", LocalDate.now(), "fe",1,false,categories);
+        Task task = new Task("test", "test", LocalDate.now(), "fe", 1, false, categories);
         addTask(task, sunDayContainer);
     }
-    
+
 }
